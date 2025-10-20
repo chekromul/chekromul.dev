c9e842ade39219b121760b7c611bb615805eba67
Roman Chekurov <chekromul@gmail.com>
2025-10-20 14:34:13 +0200

    chore: remove  `lightningcss`

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,8 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  css: {
-    transformer: "lightningcss",
-    lightningcss: {
-      drafts: {
-        customMedia: true,
-        nesting: true,
-      },
-    },
-  },
   build: {
-    cssMinify: "lightningcss",
     target: "esnext",
-    cssTarget: ["chrome120", "safari17", "firefox121", "edge120"],
   },
   plugins: [
     createHtmlPlugin({
